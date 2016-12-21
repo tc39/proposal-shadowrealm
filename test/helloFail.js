e8339566ee08d277a4d7cce92e8165621b4964ce
Dan Connolly <dckc@madmode.com>
2016-12-20 21:06:32 -0600

    exercise test262 harness for proposal-realms
    trivial passing, failing tests and minimal README

---
+++ test/helloFail.js
@@ -0,0 +1,8 @@
+// Copyright (C) 2016 Dan Connolly. All rights reserved.
+// This code is governed by the BSD license found in the LICENSE file.
+
+/*---
+ description: experimental failing test
+---*/
+
+throw('you lose!');
---
+++ test/helloPass.js
@@ -0,0 +1,9 @@
+// Copyright (C) 2016 Dan Connolly. All rights reserved.
+// This code is governed by the BSD license found in the LICENSE file.
+
+/*---
+ description: experimental passing test
+---*/
+
+'you win!';
+
