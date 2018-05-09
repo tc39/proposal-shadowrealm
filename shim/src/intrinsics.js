d4c416cd452ed5e2c916841f215de9bebefaf6c3
JF Paradis <jparadis@salesforce.com>
2018-05-09 00:17:11 -0700

    Fix intrinsics

--- shim/src/intrinsics.js
+++ shim/src/intrinsics.js
@@ -1,294 +1,299 @@
-import Realm from "./realm";
-import { getPrototypeOf } from "./commons";
+import Realm from './realm';
+import { getPrototypeOf } from './commons';
 
 /**
  * Get the intrinsics from Table 7 & Annex B
+ * Named intrinsics: available as data properties of the global object.
+ * Anonymous intrinsics: not otherwise reachable by own property name traversal.
+ *
  * https://tc39.github.io/ecma262/#table-7
  * https://tc39.github.io/ecma262/#table-73
  */
 export function getIntrinsics(sandbox) {
-    const { confinedWindow: global } = sandbox;
+  const { unsafeGlobal: g } = sandbox;
 
-    // Anonymous intrinsics.
+  // Anonymous intrinsics.
 
-    const SymbolIterator = (typeof global.Symbol && global.Symbol.iterator) || "@@iterator";
+  const SymbolIterator = g.Symbol.iterator;
 
-    const ArrayIteratorInstance = new global.Array()[SymbolIterator]();
-    const ArrayIteratorPrototype = getPrototypeOf(ArrayIteratorInstance);
-    const IteratorPrototype = getPrototypeOf(ArrayIteratorPrototype);
+  const ArrayIteratorInstance = new g.Array()[SymbolIterator]();
+  const ArrayIteratorPrototype = getPrototypeOf(ArrayIteratorInstance);
+  const IteratorPrototype = getPrototypeOf(ArrayIteratorPrototype);
 
-    const AsyncFunctionInstance = global.eval("(async function(){})");
-    const AsyncFunction = AsyncFunctionInstance.constructor;
-    const AsyncFunctionPrototype = AsyncFunction.prototype;
+  const AsyncFunctionInstance = g.eval('(async function(){})');
+  const AsyncFunction = AsyncFunctionInstance.constructor;
+  const AsyncFunctionPrototype = AsyncFunction.prototype;
 
-    const GeneratorFunctionInstance = global.eval("(function*(){})");
-    const GeneratorFunction = GeneratorFunctionInstance.constructor;
-    const Generator = GeneratorFunction.prototype;
-    const GeneratorPrototype = Generator.prototype;
+  const GeneratorFunctionInstance = g.eval('(function*(){})');
+  const GeneratorFunction = GeneratorFunctionInstance.constructor;
+  const Generator = GeneratorFunction.prototype;
+  const GeneratorPrototype = Generator.prototype;
 
-    let AsyncGeneratorFunctionInstance;
-    try {
-        AsyncGeneratorFunctionInstance = global.eval('(async function*(){})');
-    } catch(e) {
-      /* unsupported */
-    }
-    const AsyncGeneratorFunction = AsyncGeneratorFunctionInstance && AsyncGeneratorFunctionInstance.constructor;
-    const AsyncGenerator = AsyncGeneratorFunctionInstance && AsyncGeneratorFunction.prototype;
-    const AsyncGeneratorPrototype = AsyncGeneratorFunctionInstance && AsyncGenerator.prototype;
+  const hasAsyncIterator = typeof g.Symbol.asyncIterator !== 'undefined';
 
-    const AsyncFromSyncIteratorPrototype = AsyncGeneratorFunctionInstance && undefined; // TODO
-    const AsyncIteratorPrototype = AsyncGeneratorFunctionInstance && getPrototypeOf(AsyncGeneratorPrototype);
+  const AsyncGeneratorFunctionInstance = hasAsyncIterator && g.eval('(async function*(){})');
+  const AsyncGeneratorFunction = hasAsyncIterator && AsyncGeneratorFunctionInstance.constructor;
+  const AsyncGenerator = hasAsyncIterator && AsyncGeneratorFunction.prototype;
+  const AsyncGeneratorPrototype = hasAsyncIterator && AsyncGenerator.prototype;
 
-    const MapIteratorObject = new global.Map()[SymbolIterator]();
-    const MapIteratorPrototype = getPrototypeOf(MapIteratorObject);
+  const AsyncIteratorPrototype = hasAsyncIterator && getPrototypeOf(AsyncGeneratorPrototype);
+  const AsyncFromSyncIteratorPrototype = undefined; // Not reacheable.
 
-    const SetIteratorObject = new global.Set()[SymbolIterator]();
-    const SetIteratorPrototype = getPrototypeOf(SetIteratorObject);
+  const MapIteratorObject = new g.Map()[SymbolIterator]();
+  const MapIteratorPrototype = getPrototypeOf(MapIteratorObject);
 
-    const StringIteratorObject = new global.String()[SymbolIterator]();
-    const StringIteratorPrototype = getPrototypeOf(StringIteratorObject);
+  const SetIteratorObject = new g.Set()[SymbolIterator]();
+  const SetIteratorPrototype = getPrototypeOf(SetIteratorObject);
 
-    const ThrowTypeError = global.eval(
-        '(function () { "use strict"; return Object.getOwnPropertyDescriptor(arguments, "callee").get; })()'
-    );
+  const StringIteratorObject = new g.String()[SymbolIterator]();
+  const StringIteratorPrototype = getPrototypeOf(StringIteratorObject);
 
-    const TypedArray = getPrototypeOf(Int8Array);
-    const TypedArrayPrototype = TypedArray.prototype;
+  const ThrowTypeError = g.eval(
+    '(function () { "use strict"; return Object.getOwnPropertyDescriptor(arguments, "callee").get; })()'
+  );
 
-    // Named intrinsics
+  const TypedArray = getPrototypeOf(g.Int8Array);
+  const TypedArrayPrototype = g.TypedArray.prototype;
 
-    return {
-        // *** Table 7
+  // Named intrinsics
 
-        // %Array%
-        Array: global.Array,
-        // %ArrayBuffer%
-        ArrayBuffer: global.ArrayBuffer,
-        // %ArrayBufferPrototype%
-        ArrayBufferPrototype: global.ArrayBuffer.prototype,
-        // %ArrayIteratorPrototype%
-        ArrayIteratorPrototype,
-        // %ArrayPrototype%
-        ArrayPrototype: global.Array.prototype,
-        // %ArrayProto_entries%
-        ArrayProto_entries: global.Array.prototype.entries,
-        // %ArrayProto_foreach%
-        ArrayProto_foreach: global.Array.prototype.forEach,
-        // %ArrayProto_keys%
-        ArrayProto_keys: global.Array.prototype.forEach,
-        // %ArrayProto_values%
-        ArrayProto_values: global.Array.prototype.values,
-        // %AsyncFromSyncIteratorPrototype%
-        AsyncFromSyncIteratorPrototype,
-        // %AsyncFunction%
-        AsyncFunction,
-        // %AsyncFunctionPrototype%
-        AsyncFunctionPrototype,
-        // %AsyncGenerator%
-        AsyncGenerator,
-        // %AsyncGeneratorFunction%
-        AsyncGeneratorFunction,
-        // %AsyncGeneratorPrototype%
-        AsyncGeneratorPrototype,
-        // %AsyncIteratorPrototype%
-        AsyncIteratorPrototype,
-        // %Atomics%
-        Atomics: global.Atomics,
-        // %Boolean%
-        Boolean: global.Boolean,
-        // %BooleanPrototype%
-        BooleanPrototype: global.Boolean.prototype,
-        // %DataView%
-        DataView: global.DataView,
-        // %DataViewPrototype%
-        DataViewPrototype: global.DataView.prototype,
-        // %Date%
-        Date: global.Date,
-        // %DatePrototype%
-        DatePrototype: global.Date.prototype,
-        // %decodeURI%
-        decodeURI: global.decodeURI,
-        // %decodeURIComponent%
-        decodeURIComponent: global.decodeURIComponent,
-        // %encodeURI%
-        encodeURI: global.encodeURI,
-        // %encodeURIComponent%
-        encodeURIComponent: global.encodeURIComponent,
-        // %Error%
-        Error: global.Error,
-        // %ErrorPrototype%
-        ErrorPrototype: global.Error.prototype,
-        // %eval%
-        eval: sandbox.eval,
-        // %EvalError%
-        EvalError: global.EvalError,
-        // %EvalErrorPrototype%
-        EvalErrorPrototype: global.EvalError.prototype,
-        // %Float32Array%
-        Float32Array: global.Float32Array,
-        // %Float32ArrayPrototype%
-        Float32ArrayPrototype: global.Float32Array.prototype,
-        // %Float64Array%
-        Float64Array: global.Float64Array,
-        // %Float64ArrayPrototype%
-        Float64ArrayPrototype: global.Float64Array.prototype,
-        // %Function%
-        Function: sandbox.Function,
-        // %FunctionPrototype%
-        FunctionPrototype: sandbox.Function.prototype,
-        // %Generator%
-        Generator,
-        // %GeneratorFunction%
-        GeneratorFunction,
-        // %GeneratorPrototype%
-        GeneratorPrototype,
-        // %Int8Array%
-        Int8Array: global.Int8Array,
-        // %Int8ArrayPrototype%
-        Int8ArrayPrototype: global.Int8Array.prototype,
-        // %Int16Array%
-        Int16Array: global.Int16Array,
-        // %Int16ArrayPrototype%,
-        Int16ArrayPrototype: global.Int16Array.prototype,
-        // %Int32Array%
-        Int32Array: global.Int32Array,
-        // %Int32ArrayPrototype%
-        Int32ArrayPrototype: global.Int32Array.prototype,
-        // %isFinite%
-        isFinite: global.isFinite,
-        // %isNaN%
-        isNaN: global.isNaN,
-        // %IteratorPrototype%
-        IteratorPrototype,
-        // %JSON%
-        JSON: global.JSON,
-        // %JSONParse%
-        JSONParse: global.JSON.parse,
-        // %Map%
-        Map: global.Map,
-        // %MapIteratorPrototype%
-        MapIteratorPrototype,
-        // %MapPrototype%
-        MapPrototype: global.Map.prototype,
-        // %Math%
-        Math: global.Math,
-        // %Number%
-        Number: global.Number,
-        // %NumberPrototype%
-        NumberPrototype: global.Number.prototype,
-        // %Object%
-        Object: global.Object,
-        // %ObjectPrototype%
-        ObjectPrototype: global.Object.prototype,
-        // %ObjProto_toString%
-        ObjProto_toString: global.Object.prototype.toString,
-        // %ObjProto_valueOf%
-        ObjProto_valueOf: global.Object.prototype.valueOf,
-        // %parseFloat%
-        parseFloat: global.parseFloat,
-        // %parseInt%
-        parseInt: global.parseInt,
-        // %Promise%
-        Promise: global.Promise,
-        // %Promise_all%
-        Promise_all: global.Promise.all,
-        // %Promise_reject%
-        Promise_reject: global.Promise.reject,
-        // %Promise_resolve%
-        Promise_resolve: global.Promise.resolve,
-        // %PromiseProto_then%
-        PromiseProto_then: global.Promise.prototype.then,
-        // %PromisePrototype%
-        PromisePrototype: global.Promise.prototype,
-        // %Proxy%
-        Proxy: global.Proxy,
-        // %RangeError%
-        RangeError: global.RangeError,
-        // %RangeErrorPrototype%
-        RangeErrorPrototype: global.RangeError.prototype,
-        // %ReferenceError%
-        ReferenceError: global.ReferenceError,
-        // %ReferenceErrorPrototype%
-        ReferenceErrorPrototype: global.ReferenceError.prototype,
-        // %Reflect%
-        Reflect: global.Reflect,
-        // %RegExp%
-        RegExp: global.RegExp,
-        // %RegExpPrototype%
-        RegExpPrototype: global.RegExp.prototype,
-        // %Set%
-        Set: global.Set,
-        // %SetIteratorPrototype%
-        SetIteratorPrototype,
-        // %SetPrototype%
-        SetPrototype: global.Set.prototype,
-        // %SharedArrayBuffer%
-        // SharedArrayBuffer: undefined, // Deprecated on Jan 5, 2018
-        // %SharedArrayBufferPrototype%
-        // SharedArrayBufferPrototype: undefined, // Deprecated on Jan 5, 2018
-        // %String%
-        String: global.String,
-        // %StringIteratorPrototype%
-        StringIteratorPrototype,
-        // %StringPrototype%
-        StringPrototype: global.String.prototype,
-        // %Symbol%
-        Symbol: global.Symbol,
-        // %SymbolPrototype%
-        SymbolPrototype: global.Symbol.prototype,
-        // %SyntaxError%
-        SyntaxError: global.SyntaxError,
-        // %SyntaxErrorPrototype%
-        SyntaxErrorPrototype: global.SyntaxError.prototype,
-        // %ThrowTypeError%
-        ThrowTypeError,
-        // %TypedArray%
-        TypedArray,
-        // %TypedArrayPrototype%
-        TypedArrayPrototype,
-        // %TypeError%
-        TypeError: global.TypeError,
-        // %TypeErrorPrototype%
-        TypeErrorPrototype: global.TypeError.prototype,
-        // %Uint8Array%
-        Uint8Array: global.Uint8Array,
-        // %Uint8ArrayPrototype%
-        Uint8ArrayPrototype: global.Uint8Array.prototype,
-        // %Uint8ClampedArray%
-        Uint8ClampedArray: global.Uint8ClampedArray,
-        // %Uint8ClampedArrayPrototype%
-        Uint8ClampedArrayPrototype: global.Uint8ClampedArray.prototype,
-        // %Uint16Array%
-        Uint16Array: global.Uint16Array,
-        // %Uint16ArrayPrototype%
-        Uint16ArrayPrototype: Uint16Array.prototype,
-        // %Uint32Array%
-        Uint32Array: global.Uint32Array,
-        // %Uint32ArrayPrototype%
-        Uint32ArrayPrototype: global.Uint32Array.prototype,
-        // %URIError%
-        URIError: global.URIError,
-        // %URIErrorPrototype%
-        URIErrorPrototype: global.URIError.prototype,
-        // %WeakMap%
-        WeakMap: global.WeakMap,
-        // %WeakMapPrototype%
-        WeakMapPrototype: global.WeakMap.prototype,
-        // %WeakSet%
-        WeakSet: global.WeakSet,
-        // %WeakSetPrototype%
-        WeakSetPrototype: global.WeakSet.prototype,
+  const intrinsics = {
+    // *** Table 7
 
-        // *** Annex B
+    // %Array%
+    Array: g.Array,
+    // %ArrayBuffer%
+    ArrayBuffer: g.ArrayBuffer,
+    // %ArrayBufferPrototype%
+    ArrayBufferPrototype: g.ArrayBuffer.prototype,
+    // %ArrayIteratorPrototype%
+    ArrayIteratorPrototype,
+    // %ArrayPrototype%
+    ArrayPrototype: g.Array.prototype,
+    // %ArrayProto_entries%
+    ArrayProto_entries: g.Array.prototype.entries,
+    // %ArrayProto_foreach%
+    ArrayProto_foreach: g.Array.prototype.forEach,
+    // %ArrayProto_keys%
+    ArrayProto_keys: g.Array.prototype.forEach,
+    // %ArrayProto_values%
+    ArrayProto_values: g.Array.prototype.values,
+    // %AsyncFromSyncIteratorPrototype%
+    AsyncFromSyncIteratorPrototype,
+    // %AsyncFunction%
+    AsyncFunction,
+    // %AsyncFunctionPrototype%
+    AsyncFunctionPrototype,
+    // %AsyncGenerator%
+    AsyncGenerator,
+    // %AsyncGeneratorFunction%
+    AsyncGeneratorFunction,
+    // %AsyncGeneratorPrototype%
+    AsyncGeneratorPrototype,
+    // %AsyncIteratorPrototype%
+    AsyncIteratorPrototype,
+    // %Atomics%
+    Atomics: g.Atomics,
+    // %Boolean%
+    Boolean: g.Boolean,
+    // %BooleanPrototype%
+    BooleanPrototype: g.Boolean.prototype,
+    // %DataView%
+    DataView: g.DataView,
+    // %DataViewPrototype%
+    DataViewPrototype: g.DataView.prototype,
+    // %Date%
+    Date: g.Date,
+    // %DatePrototype%
+    DatePrototype: g.Date.prototype,
+    // %decodeURI%
+    decodeURI: g.decodeURI,
+    // %decodeURIComponent%
+    decodeURIComponent: g.decodeURIComponent,
+    // %encodeURI%
+    encodeURI: g.encodeURI,
+    // %encodeURIComponent%
+    encodeURIComponent: g.encodeURIComponent,
+    // %Error%
+    Error: g.Error,
+    // %ErrorPrototype%
+    ErrorPrototype: g.Error.prototype,
+    // %eval%
+    eval: sandbox.eval,
+    // %EvalError%
+    EvalError: g.EvalError,
+    // %EvalErrorPrototype%
+    EvalErrorPrototype: g.EvalError.prototype,
+    // %Float32Array%
+    Float32Array: g.Float32Array,
+    // %Float32ArrayPrototype%
+    Float32ArrayPrototype: g.Float32Array.prototype,
+    // %Float64Array%
+    Float64Array: g.Float64Array,
+    // %Float64ArrayPrototype%
+    Float64ArrayPrototype: g.Float64Array.prototype,
+    // %Function%
+    Function: sandbox.Function,
+    // %FunctionPrototype%
+    FunctionPrototype: sandbox.Function.prototype,
+    // %Generator%
+    Generator,
+    // %GeneratorFunction%
+    GeneratorFunction,
+    // %GeneratorPrototype%
+    GeneratorPrototype,
+    // %Int8Array%
+    Int8Array: g.Int8Array,
+    // %Int8ArrayPrototype%
+    Int8ArrayPrototype: g.Int8Array.prototype,
+    // %Int16Array%
+    Int16Array: g.Int16Array,
+    // %Int16ArrayPrototype%,
+    Int16ArrayPrototype: g.Int16Array.prototype,
+    // %Int32Array%
+    Int32Array: g.Int32Array,
+    // %Int32ArrayPrototype%
+    Int32ArrayPrototype: g.Int32Array.prototype,
+    // %isFinite%
+    isFinite: g.isFinite,
+    // %isNaN%
+    isNaN: g.isNaN,
+    // %IteratorPrototype%
+    IteratorPrototype,
+    // %JSON%
+    JSON: g.JSON,
+    // %JSONParse%
+    JSONParse: g.JSON.parse,
+    // %Map%
+    Map: g.Map,
+    // %MapIteratorPrototype%
+    MapIteratorPrototype,
+    // %MapPrototype%
+    MapPrototype: g.Map.prototype,
+    // %Math%
+    Math: g.Math,
+    // %Number%
+    Number: g.Number,
+    // %NumberPrototype%
+    NumberPrototype: g.Number.prototype,
+    // %Object%
+    Object: g.Object,
+    // %ObjectPrototype%
+    ObjectPrototype: g.Object.prototype,
+    // %ObjProto_toString%
+    ObjProto_toString: g.Object.prototype.toString,
+    // %ObjProto_valueOf%
+    ObjProto_valueOf: g.Object.prototype.valueOf,
+    // %parseFloat%
+    parseFloat: g.parseFloat,
+    // %parseInt%
+    parseInt: g.parseInt,
+    // %Promise%
+    Promise: g.Promise,
+    // %Promise_all%
+    Promise_all: g.Promise.all,
+    // %Promise_reject%
+    Promise_reject: g.Promise.reject,
+    // %Promise_resolve%
+    Promise_resolve: g.Promise.resolve,
+    // %PromiseProto_then%
+    PromiseProto_then: g.Promise.prototype.then,
+    // %PromisePrototype%
+    PromisePrototype: g.Promise.prototype,
+    // %Proxy%
+    Proxy: g.Proxy,
+    // %RangeError%
+    RangeError: g.RangeError,
+    // %RangeErrorPrototype%
+    RangeErrorPrototype: g.RangeError.prototype,
+    // %ReferenceError%
+    ReferenceError: g.ReferenceError,
+    // %ReferenceErrorPrototype%
+    ReferenceErrorPrototype: g.ReferenceError.prototype,
+    // %Reflect%
+    Reflect: g.Reflect,
+    // %RegExp%
+    RegExp: g.RegExp,
+    // %RegExpPrototype%
+    RegExpPrototype: g.RegExp.prototype,
+    // %Set%
+    Set: g.Set,
+    // %SetIteratorPrototype%
+    SetIteratorPrototype,
+    // %SetPrototype%
+    SetPrototype: g.Set.prototype,
+    // %SharedArrayBuffer%
+    // SharedArrayBuffer - Deprecated on Jan 5, 2018
+    // %SharedArrayBufferPrototype%
+    // SharedArrayBufferPrototype - Deprecated on Jan 5, 2018
+    // %String%
+    String: g.String,
+    // %StringIteratorPrototype%
+    StringIteratorPrototype,
+    // %StringPrototype%
+    StringPrototype: g.String.prototype,
+    // %Symbol%
+    Symbol: g.Symbol,
+    // %SymbolPrototype%
+    SymbolPrototype: g.Symbol.prototype,
+    // %SyntaxError%
+    SyntaxError: g.SyntaxError,
+    // %SyntaxErrorPrototype%
+    SyntaxErrorPrototype: g.SyntaxError.prototype,
+    // %ThrowTypeError%
+    ThrowTypeError,
+    // %TypedArray%
+    TypedArray,
+    // %TypedArrayPrototype%
+    TypedArrayPrototype,
+    // %TypeError%
+    TypeError: g.TypeError,
+    // %TypeErrorPrototype%
+    TypeErrorPrototype: g.TypeError.prototype,
+    // %Uint8Array%
+    Uint8Array: g.Uint8Array,
+    // %Uint8ArrayPrototype%
+    Uint8ArrayPrototype: g.Uint8Array.prototype,
+    // %Uint8ClampedArray%
+    Uint8ClampedArray: g.Uint8ClampedArray,
+    // %Uint8ClampedArrayPrototype%
+    Uint8ClampedArrayPrototype: g.Uint8ClampedArray.prototype,
+    // %Uint16Array%
+    Uint16Array: g.Uint16Array,
+    // %Uint16ArrayPrototype%
+    Uint16ArrayPrototype: g.Uint16Array.prototype,
+    // %Uint32Array%
+    Uint32Array: g.Uint32Array,
+    // %Uint32ArrayPrototype%
+    Uint32ArrayPrototype: g.Uint32Array.prototype,
+    // %URIError%
+    URIError: g.URIError,
+    // %URIErrorPrototype%
+    URIErrorPrototype: g.URIError.prototype,
+    // %WeakMap%
+    WeakMap: g.WeakMap,
+    // %WeakMapPrototype%
+    WeakMapPrototype: g.WeakMap.prototype,
+    // %WeakSet%
+    WeakSet: g.WeakSet,
+    // %WeakSetPrototype%
+    WeakSetPrototype: g.WeakSet.prototype,
 
-        // %escape%
-        escape: global.escape,
-        // %unescape%
-        unescape: global.unescape,
+    // *** Annex B
 
-        // TODO: Other special cases
+    // %escape%
+    escape: g.escape,
+    // %unescape%
+    unescape: g.unescape,
 
-        // *** ESNext
-        Realm // intentionally passing around the Realm Constructor, which could be used as a side channel, but still!
-    };
+    // *** ECMA-402
+
+    Intl: g.Intl,
+
+    // *** ESNext
+
+    Realm // intentionally passing around the Realm Constructor, which could be used as a side channel, but still!
+  };
+
+  return intrinsics;
 }
