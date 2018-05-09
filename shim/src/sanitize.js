8841a6e138706dc08631183c89e30bd08b6db048
JF Paradis <jparadis@salesforce.com>
2018-05-09 00:36:07 -0700

    Fix typos

--- shim/src/sanitize.js
+++ shim/src/sanitize.js
@@ -1,10 +1,10 @@
 import { repairAccessors } from './accessors';
 import { repairFunctions } from './functions';
 
-// Sanitizing ensures that the neither the legacy
+// Sanitizing ensures that neither the legacy
 // accessors nor the function constructors can be
 // used to escape the confinement of the evaluators
-// and execure in the sandbox.
+// to execute in the sandbox.
 
 export function sanitize(sandbox) {
   repairAccessors(sandbox);
