cd929c35af56226c4c0f2d0b54ed815ae449b96b
JF Paradis <jparadis@salesforce.com>
2018-05-25 02:04:27 -0700

    Added tests

---
+++ shim/test/test262/built-ins/Array/slice/create-proto-from-ctor-realm-non-array.js
@@ -0,0 +1,25 @@
+import test from 'tape';
+import Realm from '../../../../../src/realm';
+
+test('esid: sec-array.prototype.slice', t => {
+  t.plan(2);
+
+  const array = [];
+  let callCount = 0;
+  const CustomCtor = function() {};
+  const OObject = new Realm().global.Object;
+  const speciesDesc = {
+    get() {
+      callCount += 1;
+    }
+  };
+  array.constructor = OObject;
+  OObject[Symbol.species] = CustomCtor;
+
+  Object.defineProperty(Array, Symbol.species, speciesDesc);
+
+  const result = array.filter(() => undefined);
+
+  t.equal(Object.getPrototypeOf(result), CustomCtor.prototype);
+  t.equal(callCount, 0, 'Array species constructor is not referenced');
+});
