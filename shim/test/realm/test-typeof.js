43410d0173c4e4c643352b0ab831c9cf3262db20
Brian Warner <warner@lothar.com>
2018-06-18 12:33:03 -0700

    new test: typeof in the realm should handle censored globals properly (#113)
    * new test: typeof in the realm should handle censored globals properly
    
    * update test to use 'console' instead of 'process'
    
    and fix the fact that we can't currently censor this properly
    
    * test: handle Node 7 by checking to see if 'console' is present

---
+++ shim/test/realm/test-typeof.js
@@ -0,0 +1,30 @@
+import test from 'tape';
+import Realm from '../../src/realm';
+import { createContextRec } from '../../src/context';
+
+test('typeof', t => {
+  t.throws(() => DEFINITELY_NOT_DEFINED, ReferenceError); // eslint-disable-line
+  t.equal(typeof DEFINITELY_NOT_DEFINED, 'undefined'); // eslint-disable-line
+  t.equal(typeof 4, 'number');
+  t.equal(typeof undefined, 'undefined');
+  t.equal(typeof 'a string', 'string');
+  t.ok(console);
+  t.equal(typeof console, 'object');
+
+  const r = new Realm();
+  t.throws(() => r.evaluate('DEFINITELY_NOT_DEFINED'), r.global.ReferenceError);
+  t.equal(r.evaluate('typeof DEFINITELY_NOT_DEFINED'), 'undefined');
+  t.equal(r.evaluate('typeof 4'), 'number');
+  t.equal(r.evaluate('typeof undefined'), 'undefined');
+  t.equal(r.evaluate('typeof "a string"'), 'string');
+  // todo: the Realm currently censors objects from the unsafe global, but
+  // they appear 'undefined' rather than throwing a ReferenceError
+  // t.throws(() => r.evaluate('console'), r.global.ReferenceError);
+
+  // node 7 doesn't have 'console' in the vm environment
+  if ('console' in createContextRec().contextGlobal) {
+    t.equal(r.evaluate('console'), undefined); // should be censored
+    t.equal(r.evaluate('typeof console'), 'undefined'); // should be censored
+  }
+  t.end();
+});
