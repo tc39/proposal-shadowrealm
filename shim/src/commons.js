0f3de714a8e70cdfd44a21ee6e8d244e1dbeb8e9
Brian Warner <warner@lothar.com>
2018-06-21 11:41:55 -0700

    add review notes

--- shim/src/commons.js
+++ shim/src/commons.js
@@ -2,6 +2,10 @@
 // improves both consistency and minification. Unused declarations are
 // dropped by the tree shaking process.
 
+// we capture these, not just for brevity, but for security. If any code
+// modifies Object to change what 'assign' points to, the Realm shim would be
+// corrupted.
+
 export const {
   assign,
   create,
@@ -21,9 +25,12 @@ export const {
 } = Reflect;
 
 // See http://wiki.ecmascript.org/doku.php?id=conventions:safe_meta_programming
+// which only lives at http://web.archive.org/web/20160805225710/http://wiki.ecmascript.org/doku.php?id=conventions:safe_meta_programming
 const bind = Function.prototype.bind;
 const uncurryThis = bind.bind(bind.call);
 
+// We also capture these for security: changes to Array.prototype after the
+// Realm shim runs shouldn't affect subsequent Realm operations.
 export const objectHasOwnProperty = uncurryThis(Object.prototype.hasOwnProperty),
   arrayPush = uncurryThis(Array.prototype.push),
   arrayPop = uncurryThis(Array.prototype.pop),
