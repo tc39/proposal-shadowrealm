14dfd421eb81549377262c595d3deb2cfe71a33f
Brian Warner <warner@lothar.com>
2018-07-02 13:38:50 -0700

    reject dynamic import expressions in eval() and new Function()
    We originally considered doing this as part of a shim that would be injected
    into each new RootRealm (wrapping the eval/Function globals), but there are
    too many cases to do this easily. Now we just apply it in all RootRealms when
    building the safe evaluator.
    
    We use a conservative regexp to spot 'import' keywords. This will reject
    several safe forms, including commented out import() calls (making them
    taboo: even talking about a dynamic import will cause the script to be
    rejected). If/when a full parser is available, we should be able to make this
    safe and accurate.
    
    We do not look for `import.`, since 'import.meta' is not allowed in script
    contexts, so we don't need to guard against it.
    
    The security of this regexp depends it matching the behavior of the parser,
    specifically they must agree on what qualifies as whitespace. There is a test
    case that exhaustively checks all unicode code points for agreement, but it
    takes too long to every time (35s on my laptop), so it's disabled. We should
    run it at least once on any new platform to make sure there are no surprises.

---
+++ shim/src/block-imports.js
@@ -0,0 +1,19 @@
+// this \s *must* match all kinds of syntax-defined whitespace. If e.g.
+// U+2028 (LINE SEPARATOR) or U+2029 (PARAGRAPH SEPARATOR) is treated as
+// whitespace by the parser, but not matched by /\s/, then this would admit
+// an attack like: import\u2028('power.js') . We're trying to distinguish
+// something like that from something like importnotreally('power.js') which
+// is perfectly safe.
+
+const scanner = /^(.*)\bimport\s*(\(|\/\/|\/\*)/m;
+
+export function rejectImportExpressions(s) {
+  const matches = scanner.exec(s);
+  if (matches) {
+    // todo: if we have a full parser available, use it here. If there is no
+    // 'import' token in the string, we're safe.
+    // if (!parse(s).contains('import')) return;
+    const linenum = matches[1].split('\n').length; // more or less
+    throw new SyntaxError(`possible import expression rejected around line ${linenum}`);
+  }
+}
