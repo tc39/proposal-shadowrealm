2fcc2010148c87b2f7a497dd1919b045444196d4
Jean-Francois Paradis <jf@pardis.com>
2018-07-07 08:58:49 -0700

    Console.error tantrums

--- shim/test/realm/utilities.js
+++ shim/test/realm/utilities.js
@@ -1,15 +1,47 @@
 import test from 'tape';
+import sinon from 'sinon';
 import { throwTantrum, assert } from '../../src/utilities';
 
+/* eslint-disable no-console */
+
 test('throwTantrum', t => {
-  t.plan(1);
+  t.plan(3);
+
+  sinon.stub(console, 'error').callsFake();
 
   t.throws(() => throwTantrum('foo'), /^please report internal shim error: foo$/);
+
+  t.equals(console.error.callCount, 1);
+  t.equals(console.error.getCall(0).args[0], 'please report internal shim error: foo');
+
+  console.error.restore();
+});
+
+test('throwTantrum', t => {
+  t.plan(5);
+
+  sinon.stub(console, 'error');
+
+  t.throws(() => throwTantrum('foo', new Error('bar')), /^please report internal shim error: foo$/);
+
+  t.equals(console.error.callCount, 3);
+  t.equals(console.error.getCall(0).args[0], 'please report internal shim error: foo');
+  t.equals(console.error.getCall(1).args[0], 'Error: bar');
+  t.ok(console.error.getCall(2).args[0].includes('at t.throws'));
+
+  console.error.restore();
 });
 
 test('assert', t => {
-  t.plan(2);
+  t.plan(4);
+
+  sinon.stub(console, 'error').callsFake();
 
   t.doesNotThrow(() => assert(true, 'foo'));
   t.throws(() => assert(false, 'foo'), /^please report internal shim error: failed to: foo$/);
+
+  t.equals(console.error.callCount, 1);
+  t.equals(console.error.getCall(0).args[0], 'please report internal shim error: failed to: foo');
+
+  console.error.restore();
 });
