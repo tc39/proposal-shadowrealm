e68c8d36cd3578ce6c58d77d2d61eb109c036a48
JF Paradis <jparadis@salesforce.com>
2018-07-14 14:37:27 -0700

    build

--- shim/dist/realm-shim.js
+++ shim/dist/realm-shim.js
@@ -463,8 +463,6 @@
     function repairFunction(name, declaration) {
       let FunctionInstance;
       try {
-        // Use Function() because eval() has issues with serializing functions under the esm module.
-        // TODO: investigate esm distortion of source code.
         // eslint-disable-next-line no-new-func
         FunctionInstance = (0, eval)(declaration);
       } catch (e) {
@@ -615,120 +613,143 @@
     return createUnsafeRec(unsafeGlobal, []);
   }
 
-  // the ScopeHandler manages a Proxy which serves as the global scope for the
-
-  class ScopeHandler {
-    // Properties stored on the handler are not available from the proxy.
-
-    // the Proxy is only used by with(), so the Handler only needs to implement
-    // a few properties: has, get, set (which we leave at the default)
-
-    // todo: throw if any traps other than get/set/has are run (e.g.
-    // getOwnPropertyDescriptors, apply, getPrototypeOf) . Make this handler
-    // inherit from a second one whose 'get' property always throws.
-
-    constructor(unsafeRec) {
-      this.unsafeGlobal = unsafeRec.unsafeGlobal;
-      this.unsafeEval = unsafeRec.unsafeEval;
-
-      // this flag allow us to determine if the eval() call is a controlled
-      // eval done by the realm's code or if it is user-land invocation, so
-      // we can react differently.
-      this.useUnsafeEvaluator = false;
-
-      // todo: this.shadowTarget = getPrototypeOf(somehow_get_target)
-    }
-
+  /**
+   * alwaysThrowHandler is a proxy handler which throws on any trap called.
+   * It's made from a proxy with a get trap that throws. Its target is
+   * an immutable (frozen) object and is safe to share.
+   */
+  const alwaysThrowHandler = new Proxy(freeze({}), {
     get(target, prop) {
-      // Special treatment for eval. The very first lookup of 'eval' gets the
-      // unsafe (real direct) eval, so it will get the lexical scope that uses
-      // the 'with' context.
-      if (prop === 'eval') {
-        // test that it is true rather than merely truthy
-        if (this.useUnsafeEvaluator === true) {
-          // reset before use
-          this.useUnsafeEvaluator = false;
-          return this.unsafeEval;
+      throwTantrum(`unexpected scope handler trap called: ${prop}`);
+    }
+  });
+
+  /**
+   * ScopeHandler manages a Proxy which serves as the global scope for the
+   * safeEvaluator operation (the Proxy is the argument of a 'with' binding).
+   * As described in createSafeEvaluator(), it has several functions:
+   * - allow the very first (and only the very first) use of 'eval' to map to
+   *   the real (unsafe) eval function, so it acts as a 'direct eval' and can
+   *    access its lexical scope (which maps to the 'with' binding, which the
+   *   ScopeHandler also controls).
+   * - ensure that all subsequent uses of 'eval' map to the safeEvaluator,
+   *   which lives as the 'eval' property of the safeGlobal.
+   * - route all other property lookups at the safeGlobal.
+   * - hide the unsafeGlobal which lives on the scope chain above the 'with'.
+   * - ensure the Proxy invariants despite some global properties being frozen.
+   */
+  function createScopeHandler(unsafeRec) {
+    const { unsafeGlobal, unsafeEval } = unsafeRec;
+
+    // This flag allow us to determine if the eval() call is an done by the
+    // realm's code or if it is user-land invocation, so we can react differently.
+    let useUnsafeEvaluator = false;
+
+    // todo (optimization): keeping a reference to the shadow to avoid calling
+    // getPrototypeOf on the target every time the get trap is invoked
+    // const shadowTarget = getPrototypeOf(somehow_get_target)
+
+    return {
+      // The scope handler throws if any trap other than get/set/has are run
+      // (e.g. getOwnPropertyDescriptors, apply, getPrototypeOf).
+      // eslint-disable-next-line no-proto
+      __proto__: alwaysThrowHandler,
+
+      allowUnsafeEvaluatorOnce() {
+        useUnsafeEvaluator = true;
+      },
+
+      unsafeEvaluatorAllowed() {
+        return useUnsafeEvaluator;
+      },
+
+      get(target, prop) {
+        // Special treatment for eval. The very first lookup of 'eval' gets the
+        // unsafe (real direct) eval, so it will get the lexical scope that uses
+        // the 'with' context.
+        if (prop === 'eval') {
+          // test that it is true rather than merely truthy
+          if (useUnsafeEvaluator === true) {
+            // revoke before use
+            useUnsafeEvaluator = false;
+            return unsafeEval;
+          }
+          return target.eval;
         }
-        return target.eval;
-      }
 
-      // todo: shim integrity, capture Symbol.unscopables
-      if (prop === Symbol.unscopables) {
-        // Safe to return a primal realm Object here because the only code that
-        // can do a get() on a non-string is the internals of with() itself,
-        // and the only thing it does is to look for properties on it. User
-        // code cannot do a lookup on non-strings.
+        // todo: shim integrity, capture Symbol.unscopables
+        if (prop === Symbol.unscopables) {
+          // Safe to return a primal realm Object here because the only code that
+          // can do a get() on a non-string is the internals of with() itself,
+          // and the only thing it does is to look for properties on it. User
+          // code cannot do a lookup on non-strings.
+          return undefined;
+        }
+
+        // Properties of the global.
+        if (prop in target) {
+          return target[prop];
+        }
+        // Prevent the lookup for other properties.
         return undefined;
-      }
+      },
 
-      // Properties of the global.
-      if (prop in target) {
-        return target[prop];
-      }
-      // Prevent the lookup for other properties.
-      return undefined;
-    }
+      // eslint-disable-next-line class-methods-use-this
+      set(target, prop, value) {
+        // todo: allow modifications when target.hasOwnProperty(prop) and it
+        // is writable, assuming we've already rejected overlap (see
+        // createSafeEvaluatorFactory.factory). This TypeError gets replaced with
+        // target[prop] = value
+        if (objectHasOwnProperty(target, prop)) {
+          // todo: shim integrity: TypeError, String
+          throw new TypeError(`do not modify endowments like ${String(prop)}`);
+        }
+        getPrototypeOf(target)[prop] = value;
 
-    // eslint-disable-next-line class-methods-use-this
-    set(target, prop, value) {
-      // Set the value on the shadow. The target itself is an empty
-      // object that is only used to prevent a frozen eval property.
-      // todo: use this.shadowTarget, for speedup
-
-      // new todo: allow modifications when target.hasOwnProperty(prop) and it
-      // is writable, assuming we've already rejected overlap (see
-      // createSafeEvaluatorFactory.factory). This TypeError gets replaced with
-      // target[prop] = value
-      if (objectHasOwnProperty(target, prop)) {
-        // todo: shim integrity: TypeError, String
-        throw new TypeError(`do not modify endowments like ${String(prop)}`);
-      }
-      getPrototypeOf(target)[prop] = value;
-      // Return true after successful set.
-      return true;
-    }
-
-    // we need has() to return false for some names to prevent the lookup  from
-    // climbing the scope chain and eventually reaching the unsafeGlobal
-    // object, which is bad.
-
-    // note: unscopables! every string in Object[Symbol.unscopables]
-
-    // todo: we'd like to just have has() return true for everything, and then
-    // use get() to raise a ReferenceError for anything not on the safe global.
-    // But we want to be compatible with ReferenceError in the normal case and
-    // the lack of ReferenceError in the 'typeof' case. Must either reliably
-    // distinguish these two cases (the trap behavior might be different), or
-    // we rely on a mandatory source-to-source transform to change 'typeof abc'
-    // to XXX. We already need a mandatory parse to prevent the 'import' and
-    // 'import.meta' expressions, since they're special forms instead of merely
-    // being a global variable
-
-    // note: if we make has() return true always, then we must implement a
-    // set() trap to avoid subverting the protection of strict mode (it would
-    // accept assignments to undefined globals, when it ought to throw
-    // ReferenceError for such assignments)
-
-    has(target, prop) {
-      // proxies stringify 'prop', so no TOCTTOU danger here
-      if (prop === 'eval') {
+        // Return true after successful set.
         return true;
-      }
-      if (prop === 'arguments') {
+      },
+
+      // we need has() to return false for some names to prevent the lookup  from
+      // climbing the scope chain and eventually reaching the unsafeGlobal
+      // object, which is bad.
+
+      // note: unscopables! every string in Object[Symbol.unscopables]
+
+      // todo: we'd like to just have has() return true for everything, and then
+      // use get() to raise a ReferenceError for anything not on the safe global.
+      // But we want to be compatible with ReferenceError in the normal case and
+      // the lack of ReferenceError in the 'typeof' case. Must either reliably
+      // distinguish these two cases (the trap behavior might be different), or
+      // we rely on a mandatory source-to-source transform to change 'typeof abc'
+      // to XXX. We already need a mandatory parse to prevent the 'import',
+      // since it's a special form instead of merely being a global variable/
+
+      // note: if we make has() return true always, then we must implement a
+      // set() trap to avoid subverting the protection of strict mode (it would
+      // accept assignments to undefined globals, when it ought to throw
+      // ReferenceError for such assignments)
+
+      has(target, prop) {
+        // proxies stringify 'prop', so no TOCTTOU danger here
+        if (prop === 'eval') {
+          return true;
+        }
+        if (prop === 'arguments') {
+          return false;
+        }
+        if (prop in target) {
+          return true;
+        }
+        // hide all properties of unsafeGlobal at the expense of 'typeof' being
+        // wrong for those properties
+        if (prop in unsafeGlobal) {
+          // in browser, 'document = 3', this will add a property to your safeGlobal
+          return true;
+        }
         return false;
       }
-      if (prop in target) {
-        return true;
-      }
-      // hide all properties of unsafeGlobal at the expense of 'typeof' being
-      // wrong for those properties
-      if (prop in this.unsafeGlobal) {
-        // in browser, 'document = 3', this will add a property to your safeGlobal
-        return true;
-      }
-      return false;
-    }
+    };
   }
 
   // this \s *must* match all kinds of syntax-defined whitespace. If e.g.
@@ -753,11 +774,10 @@
 
   // Portions adapted from V8 - Copyright 2016 the V8 project authors.
 
-  // admit many (but not all) legal variable names: starts with letter/_/$,
-  // continues with letter/digit/_/$ . It will reject many legal names that
-  // involve unicode characters. We use 'apply' rather than /../.match() in
-  // case RegExp has been poisoned.
-  const identifierPattern = /^[a-zA-Z_$][\w_$]*$/;
+  // Admits many (but not all) legal variable names: starts with letter/_/$,
+  // continues with letter/digit/_/$. It will reject many legal names that
+  // involve unicode characters. \w is equivalent [a-zA-Z_0-9]
+  const identifierPattern = /^[a-zA-Z_$][\w$]*$/;
 
   // todo: think about how this interacts with endowments, check for conflicts
   // between the names being optimized and the ones added by endowments
@@ -765,10 +785,11 @@
   function getOptimizableGlobals(safeGlobal) {
     const descs = getOwnPropertyDescriptors(safeGlobal);
 
+    // getOwnPropertyNames does ignore Symbols so we don't need this extra check:
+    // typeof name === 'string' &&
     const constants = arrayFilter(getOwnPropertyNames(descs), name => {
-      // Ensure we have a valid identifier.
-      // getOwnPropertyNames does ignore Symbols so we don't need this extra check:
-      // typeof name === 'string' &&
+      // Ensure we have a valid identifier. We use regexpMatch rather than
+      // /../.match() to guard against the case where RegExp has been poisoned.
       if (!regexpMatch(identifierPattern, name)) {
         return false;
       }
@@ -776,19 +797,21 @@
       const desc = descs[name];
       return (
         //
-        // getters will not have .writable, don't let the falsyness of
+        // The getters will not have .writable, don't let the falsyness of
         // 'undefined' trick us: test with === false, not ! . However descriptors
         // inherit from the (potentially poisoned) global object, so we might see
         // extra properties which weren't really there. Accessor properties have
         // 'get/set/enumerable/configurable', while data properties have
         // 'value/writable/enumerable/configurable'.
-        desc.configurable === true &&
-        desc.writable === true &&
+        desc.configurable === false &&
+        desc.writable === false &&
         //
-        // Check for accessor properties: we don't want to optimize these,
+        // Checks for accessor properties: we don't want to optimize these,
         // they're obviously non-constant. Value properties can't have
-        // accessors at the same time, so this check is sufficient.
-        'value' in desc
+        // accessors at the same time, so this check is sufficient. Using
+        // explicit own property deal with the case where
+        // Object.prototype has been poisoned.
+        objectHasOwnProperty(desc, 'value')
       );
     });
 
@@ -844,7 +867,7 @@
   function createSafeEvaluatorFactory(unsafeRec, safeGlobal) {
     const { unsafeFunction } = unsafeRec;
 
-    const scopeHandler = new ScopeHandler(unsafeRec);
+    const scopeHandler = createScopeHandler(unsafeRec);
     const optimizableGlobals = getOptimizableGlobals(safeGlobal);
     const scopedEvaluatorFactory = createScopedEvaluatorFactory(unsafeRec, optimizableGlobals);
 
@@ -869,7 +892,7 @@
         eval(src) {
           src = `${src}`;
           rejectImportExpressions(src);
-          scopeHandler.useUnsafeEvaluator = true;
+          scopeHandler.allowUnsafeEvaluatorOnce();
           let err;
           try {
             // Ensure that "this" resolves to the safe global.
@@ -880,10 +903,9 @@
             throw e;
           } finally {
             // belt and suspenders: the proxy switches this off immediately after
-            // the first access, but just in case we clear it here too
-            if (scopeHandler.useUnsafeEvaluator !== false) {
-              scopeHandler.useUnsafeEvaluator = false;
-              throwTantrum('handler sets useUnsafeEvaluator = false', err);
+            // the first access, but if that's not the case we abort.
+            if (scopeHandler.unsafeEvaluatorAllowed()) {
+              throwTantrum('handler did not revoke useUnsafeEvaluator', err);
             }
           }
         }
