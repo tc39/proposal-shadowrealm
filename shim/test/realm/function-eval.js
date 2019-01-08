b465a232d7df861a41264c7a5347fbd8e6ad4749
Brian Warner <warner@lothar.com>
2019-01-07 16:21:10 -0800

    fix sandbox bypass in Function constructor
    The `Function(arg1, arg2, body)` constructor works by building a function
    definition out of its parameters and then evaluating the generated string. It
    is supposed to inspect the parameters to prevent access to global variables,
    but Matt Austin found a clever bypass involving back-to-back template
    literals.
    
    This allowed execution of arbitrary code in the "safe" global environment,
    giving `Function()` authority comparable to our safe `eval()` facility. This
    wouldn't give a child Realm any additional power, but if the Realm was using
    `Function()` to test that a given untrusted string was a well-formed function
    body without actually executing it, this bug would give the source of that
    string a surprising temporal power. The attacker's expression would be
    executed immediately, whereas function constructors do not execute the
    generated function at all (they merely produce a callable value which could
    be invoked later).
    
    The attacker's expression can also access `this`, which function bodies
    cannot normally reach, but since functions can simply do `eval("this")`, this
    is not a net increase in power.
    
    Our fix is to reject all but the simplest of function parameters. We removed
    default parameters, destructuring assignment, and the "rest"
    parameter (`foo(arg1, ...rest)`), and only allow simple ASCII-based
    identifier names.
    
    refs #193

--- shim/test/realm/function-eval.js
+++ shim/test/realm/function-eval.js
@@ -41,10 +41,6 @@ test('function-injection-2', t => {
     t.throws(() => r.global.Function(...args), r.global.SyntaxError, args);
     t.equal(flag, false);
   }
-  function checkNotError(...args) {
-    r.global.Function(...args);
-    t.equal(flag, false);
-  }
 
   // test cases from https://code.google.com/archive/p/google-caja/issues/1616
   check(`}), target(), (function(){`);
@@ -53,15 +49,53 @@ test('function-injection-2', t => {
   // and from https://bugs.chromium.org/p/v8/issues/detail?id=2470
   check('/*', '*/){');
   check('', '});(function(){');
-  checkNotError('//', '//');
+  check('//', '//'); // we reject these even though that bug allows them
   check('', `});print('1+1=' + (1+1));(function(){`);
 
   // and from https://bugs.webkit.org/show_bug.cgi?id=106160
   check('){});(function(', '');
   check('', '});(function(){');
-  checkNotError('//', '//');
+  check('//', '//'); // we reject these even though that bug allows them
   check('/*', '*/){');
   check('}}; 1 * {a:{');
+
+  // bug from Matt Austin: this is surprising but doesn't allow new access
+  check('arg=`', '/*body`){});({x: this/**/');
+  // a naive evaluation might do this:
+  //     (function(arg=`){
+  //      /*body`){});({x: this/**/
+  //     })
+
+  // In which the backtick in arg= eats both the )} that we add and the /*
+  // that the body adds, allowing the body to terminate the function
+  // definition. Then the body defines a new expression, which creates an
+  // object with a property named "x" which captures the same 'this' you
+  // could have gotten with plain safe eval().
+
+  // markm tried to protect against this by injecting an extra trailing
+  // block comment to the end of the arguments, creating a body like this
+
+  //     (function(arg=`
+  //     /*``*/){
+  //      /*body`){});({x: this/**/
+  //     })
+
+  // In this version, the backtick from arg= eats the first part of the
+  // injected block comment, and the backtick from the body matches the
+  // second part of the injected block comment. That yields a
+  // syntactically-valid but semantically-invalid default argument with a
+  // value of `\n/*``*/){\n/*body` , in which the first template literal
+  // (`\n/*`) evaluates to a string ("\n/*") which is then used as the
+  // template-literal-tag for the second template literal. This is
+  // semantically invalid because strings cannot be called as functions, but
+  // the syntax is still valid. The constructed function is bypassed, so its
+  // default argument is never evaluated, so this invalidity doesn't matter.
+
+  // To protect against this, we'll just forbid everything except simple
+  // identifiers in Function constructor calls: no default arguments ("=")
+  // and no pattern matching expressions ("[a,b]"). You can still use complex
+  // arguments in function definitions, just not in calls to the Function
+  // constructor.
 
   t.end();
 });
@@ -74,27 +108,27 @@ test('function-reject-paren-default', t => {
   t.end();
 });
 
+// our shim is conservative about parameters: we only accept simple
+// identifiers; no default arguments, no pattern matching, no ...rest
+// parameters
 test('function-default-parameters', t => {
   const goodFunc = 'return a+1';
   const r = Realm.makeRootRealm();
-  const f1 = new r.global.Function('a=1', goodFunc);
-  t.equal(f1(), 2);
+  t.throws(() => new r.global.Function('a=1', goodFunc), r.global.SyntaxError);
   t.end();
 });
 
 test('function-rest-parameters', t => {
   const goodFunc = 'return rest[0] + rest[1]';
   const r = Realm.makeRootRealm();
-  const f1 = new r.global.Function('...rest', goodFunc);
-  t.equal(f1(1, 2), 3);
+  t.throws(() => new r.global.Function('...rest', goodFunc), r.global.SyntaxError);
   t.end();
 });
 
 test('function-destructuring-parameters', t => {
   const goodFunc = 'return foo + bar + baz';
   const r = Realm.makeRootRealm();
-  const f1 = new r.global.Function('{foo, bar}, baz', goodFunc);
-  t.equal(f1({ foo: 1, bar: 2 }, 3), 6);
+  t.throws(() => new r.global.Function('{foo, bar}, baz', goodFunc), r.global.SyntaxError);
   t.end();
 });
 
@@ -105,8 +139,17 @@ test('function-legitimate-but-weird-parameters', t => {
   t.equal(f1(1, 2, 3), 6);
 
   const goodFunc2 = 'return foo + bar[0] + bar[1]';
-  const f2 = new r.global.Function('foo, bar = [1', '2]', goodFunc2);
-  t.equal(f2(1), 4);
+  t.throws(() => new r.global.Function('foo, bar = [1', '2]', goodFunc2), r.global.SyntaxError);
+
+  t.end();
+});
+
+test('degenerate-pattern-match-argument', t => {
+  const r = Realm.makeRootRealm();
+  const goodFunc = 'return foo + bar + baz';
+  // this syntax is rejected by the normal JS parser, not by anything special
+  // about Realms
+  t.throws(() => new r.global.Function('3', goodFunc), r.global.SyntaxError);
 
   t.end();
 });
