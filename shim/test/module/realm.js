9c58ddd4913574cef7ffeb75d05b04284fd1e561
Kate Sills <katelynsills@gmail.com>
2019-04-01 14:24:58 -0700

    remove shim

--- shim/test/module/realm.js
+++
@@ -1,8 +0,0 @@
-import test from 'tape';
-import Realm from '../../src/realm';
-
-test('new Realm', t => {
-  t.plan(1);
-
-  t.throws(() => new Realm(), TypeError, 'new Real() should throws');
-});
